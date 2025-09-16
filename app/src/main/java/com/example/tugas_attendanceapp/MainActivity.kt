21c4c4e04ca9af9eb5e2f5ecd2986d762dcbe32c
aarmeliaa <azzahraarmrlia14@gmail.com>
2025-09-16 22:30:56 +0700

    datepicker, timepicker, dan spinner sudah

--- app/src/main/java/com/example/tugas_attendanceapp/MainActivity.kt
+++ app/src/main/java/com/example/tugas_attendanceapp/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.example.tugas_attendanceapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tugas_attendanceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var jenisPresensi: Array<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        jenisPresensi = resources.getStringArray(R.array.jenisPresensi)
+        with(binding){
+
+            datePicker.init(
+                datePicker.year,
+                datePicker.month,
+                datePicker.dayOfMonth
+            ) { _, year, monthOfYear, dayOfMonth ->
+                val selectedDate = "$dayOfMonth/${monthOfYear + 1}/$year"
+                Toast.makeText(this@MainActivity, selectedDate, Toast.LENGTH_SHORT).show()
+            }
+
+            timePicker.setOnTimeChangedListener { view, hourOfDay, minute ->
+                val selectedTime = String.format("%02d:%02d", hourOfDay, minute)
+                Toast.makeText(this@MainActivity, selectedTime, Toast.LENGTH_SHORT).show()
+            }
+
+            val adapterPresensi = ArrayAdapter(this@MainActivity,
+                android.R.layout.simple_spinner_item, jenisPresensi)
+            adapterPresensi.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerPresensi.adapter = adapterPresensi
+            spinnerPresensi.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val selectedItem = parent?.getItemAtPosition(position).toString()
+
+                    if (selectedItem == "Sakit" || selectedItem == "Terlambat" || selectedItem == "Izin") {
+                        edittextKet.visibility = View.VISIBLE
+                    } else {
+                        edittextKet.visibility = View.GONE
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+            }
         }
     }
 }
